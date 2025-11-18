c16af7f1a5186be0ce65302b8d5e2260e6e54769
9529360-cpu <9529360@gmail.com>
2025-11-19 07:14:26 +0800

    fix: qualify Application type to avoid namespace conflict

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,7 +3,7 @@ using 币安量化机器人.Services;
 
 namespace 币安量化机器人
 {
-    public partial class App : Application
+    public partial class App : global::System.Windows.Application
     {
         protected override async void OnExit(ExitEventArgs e)
         {
